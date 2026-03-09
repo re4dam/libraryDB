a461b9316ed5b65a66f644f3efe59f36c65f2385
re4dam <zakiadam332@gmail.com>
2026-03-09 15:41:39 +0700

    First Commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace LibraryConsole;
+
+
+class Program
+{
+    static readonly string connectionString = "Server=localhost;Database=LibraryDB;Integrated Security=true;TrustServerCertificate=true;";
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Library Management System!");
+
+        Database db = new Database(connectionString);
+        BookMenu bookMenu = new BookMenu(db);
+        MemberMenu memberMenu = new MemberMenu(db);
+        StaffMenu staffMenu = new StaffMenu(db);
+
+        bool running = true;
+        while(running)
+        {
+            Console.Clear();
+            Console.WriteLine("=== Main Menu ===");
+            Console.WriteLine("1. Book Management");
+            Console.WriteLine("2. Member Management");
+            Console.WriteLine("3. Staff Management");
+            Console.WriteLine("4. Exit");
+            Console.Write("Select an option: ");
+            string input = Console.ReadLine();
+            // Process input
+            switch (input)
+            {
+                case "1":
+                    bookMenu.Show(); // Delegates logic to BookMenu
+                    break;
+                case "2":
+                    memberMenu.Show(); // Delegates logic to MemberMenu
+                    break;
+                case "3":
+                    staffMenu.Show(); // Delegates logic to StaffMenu
+                    break;
+                case "4":
+                    Console.WriteLine("Exiting the system. Goodbye!");
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Press any key to try again...");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Library Management Database
+
+## Tables
+1. Members
+2. Books
+3. Staffs
+4. Loans
+
+## Features
+1. CRUD
+
+## Flow
+\Data\ is meant for the program to interact with the database
+\Model\ is meant for the program to process the data, then update the database too
